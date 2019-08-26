d74328332ec550474fa6bb31057a76d2ff3725ce
XIAOYING  HE <17.00670-8@maua.br>
2019-08-26 12:38:32 -0300

    Persistencia-SQLite

---
+++ Pratica14_sqlite_01/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import auxiliar_database.UsuariosDAO;
+import model.Usuario;
+import utilities.Constants;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        UsuariosDAO usuariosDAO;
+        usuariosDAO = new UsuariosDAO(Constants.URL_MEU_BANCO);
+        List<Usuario> meusUsuarios = usuariosDAO.getALLUsuario();
+        for (Usuario user : meusUsuarios){
+            System.out.println("ID: " + user.id);
+            System.out.println("Nome: " + user.nome);
+        }
+
+    }
+}
